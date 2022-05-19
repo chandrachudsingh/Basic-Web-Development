ece452844bf654a7401fdbb5e17741edbd6eee57
ccsc <chandrachudsingh81@gmail.com>
2022-05-19 11:45:48 +0530

    Initial commit

---
+++ JavaScript/Functions.js
@@ -0,0 +1,10 @@
+function greet(name, greetText="Greetings from JS"){
+    console.log(greetText + " " + name);
+    console.log(name + " is a good boy");
+}
+
+let name = "ccsc", name1 = "shubham", name2 = "Rohan";
+let greetText = "Good Morning";
+greet(name, greetText);
+greet(name1);
+greet(name2, greetText);
